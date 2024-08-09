ccf60c4ba165694103bdf1e0f95a35e920cb81ab
Hner1qwe <henriquetrachtafilho@gmail.com>
2024-08-09 16:07:16 -0400

    alteraçoes pelo desafio do professor

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Home from './Home'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <BrowserRoutes>
+    <Routes>
+    <Route path="/Home" element={ <home/> } />
+    <Route path="/Produto" element={ <produto/> } />
+    <Route path="/Oferta" element={ <oferta/> } />
+    </Routes>
+    </BrowserRoutes>
   </React.StrictMode>,
 )
 
