df7130eaa582fd611d5e401588733c71dd4aae91
Hner1qwe <henriquetrachtafilho@gmail.com>
2024-08-16 16:19:22 -0400

    importei os componentes

---
+++ src/Home/index.jsx
@@ -0,0 +1,7 @@
+import Header from "../componentes/Header";
+import Footer from "../componentes/Footer";
+import Oferta from "../Oferta";
+import Footer from "../componentes/Footer";
+import { useState } from "react";
+import {carousel} from "react-responsive-carousel";
+
