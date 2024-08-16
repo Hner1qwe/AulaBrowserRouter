022ff11386ab43e99ceabc6bea560f3b6bf6cdf9
Hner1qwe <henriquetrachtafilho@gmail.com>
2024-08-16 17:07:48 -0400

    link para as paginas de navegação

---
+++ src/Produto/index.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+export default function Produtos(){
+    const [Nome, setNome] = useState("");
+    const [Preco, setPreco] = useState(0);
+
+    const [ListaDeCD, setListaDeCD] = useState([]);
+
+return(
+
+)
+
+}
