c19f831ec20fc8813d7e62c70241b3ce0a555ac1
henr1qwe <henriquetrachtafilho@gmail.com>
2024-08-18 18:18:35 -0300

    tentando resolver os problemas no ListaProdutos e no Index.jsx

---
+++ src/componentes/ListaProdutos.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { link } from "react-router-dom";
+import "../globals.css"
+
+
+
+export default function ListaProdutos({ produtos }) {
+
+    return (
+        <div className="card">
+            {
+                produtos.map((objeto) => (
+                    <div calssName="container" key={objeto.id}>
+                        <img calssName="imagem" src={objeto.imagem} alt={objeto.nome} />
+                        <div className="conteudo">
+                            <p className="destaque">{objeto.nome}</p>
+                            <p className="money">{objeto.valor}</p>
+                        </div>
+                        <button className="obter">comprar</button>
+                        ))
+        }
+        </div>
+
+                );
+
