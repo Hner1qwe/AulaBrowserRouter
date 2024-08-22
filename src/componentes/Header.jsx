03250d7fcbe79098b5da8b5673f99fa18b3ce2d2
henr1qwe <henriquetrachtafilho@gmail.com>
2024-08-22 12:31:48 -0300

    fiz a paginha produtos e importei o header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,24 +1,25 @@
 import { Link } from "react-router-dom/dist";
 
+
 export default function Header(){
 
 return(
     <div className="cabeca">
-    <header>
+ 
         <nav>
             <ul>
-            <Link to="./Home">
+            <Link to="/">
                     <li>Home</li>
                 </Link>
-                <Link to="./produto">
-                    <li>Produtos</li>
+                <Link to="/produto">
+                    <li> Produtos </li>
                 </Link>
-                <Link to="./Oferta">
+                <Link to="/oferta">
                     <li>Ofertas</li>
                 </Link>
             </ul>
         </nav>
-    </header>
+  
     </div>
 
 )
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,17 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom/dist'
 import Home from './Home/Home'
+import Produto from './Produto/Produto'
+import Oferta from './Oferta/Oferta'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
-    <Route path="/Home" element={ <Home/> } />
-    <Route path="/Produto" element={ <index/> } />
-    <Route path="/Oferta" element={ <index/> } />
+    <Route path="/" element={ <Home/> } />
+    <Route path="/Produto" element={ <Produto/> } />
+    <Route path="/Oferta" element={ <Oferta/> } />
     </Routes>
     </BrowserRouter>
   </React.StrictMode>,
